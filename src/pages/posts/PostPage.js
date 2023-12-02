da253fe8f6063848e2902243da5cd121f4f03517
dEEGE2time <johannesweidensjo@hotmail.com>
2023-12-02 19:12:42 +0000

    add like and comment icons, cannot like post if owned or logged out

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,9 +7,10 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
-  const {id} = useParams();
+  const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
 
   useEffect(() => {
@@ -25,17 +26,15 @@ function PostPage() {
       }
     };
 
-    handleMount()
+    handleMount();
   }, [id]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
